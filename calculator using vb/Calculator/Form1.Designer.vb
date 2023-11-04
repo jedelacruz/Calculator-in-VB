da705dd5232f0e090fff9c7e824a69e3763a1903
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-11-04 17:55:47 +0800

    Add files via upload

---
+++ calculator using vb/Calculator/Form1.Designer.vb
@@ -0,0 +1,360 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.textBox = New System.Windows.Forms.RichTextBox()
+        Me.divideBtn = New System.Windows.Forms.Button()
+        Me.minusBtn = New System.Windows.Forms.Button()
+        Me.multiplyBtn = New System.Windows.Forms.Button()
+        Me.addBtn = New System.Windows.Forms.Button()
+        Me.equalsBtn = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btnDecimal = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.delBtn = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.delAllBtn = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'textBox
+        '
+        Me.textBox.BackColor = System.Drawing.Color.Black
+        Me.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.textBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.textBox.ForeColor = System.Drawing.Color.White
+        Me.textBox.Location = New System.Drawing.Point(17, 23)
+        Me.textBox.Name = "textBox"
+        Me.textBox.Size = New System.Drawing.Size(278, 61)
+        Me.textBox.TabIndex = 0
+        Me.textBox.Text = ""
+        '
+        'divideBtn
+        '
+        Me.divideBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(159, Byte), Integer), CType(CType(10, Byte), Integer))
+        Me.divideBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.divideBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.divideBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.divideBtn.Location = New System.Drawing.Point(238, 103)
+        Me.divideBtn.Name = "divideBtn"
+        Me.divideBtn.Size = New System.Drawing.Size(56, 56)
+        Me.divideBtn.TabIndex = 1
+        Me.divideBtn.Text = "÷"
+        Me.divideBtn.UseVisualStyleBackColor = False
+        '
+        'minusBtn
+        '
+        Me.minusBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(159, Byte), Integer), CType(CType(10, Byte), Integer))
+        Me.minusBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.minusBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.minusBtn.Location = New System.Drawing.Point(238, 252)
+        Me.minusBtn.Name = "minusBtn"
+        Me.minusBtn.Size = New System.Drawing.Size(56, 56)
+        Me.minusBtn.TabIndex = 2
+        Me.minusBtn.Text = "-"
+        Me.minusBtn.UseVisualStyleBackColor = False
+        '
+        'multiplyBtn
+        '
+        Me.multiplyBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(159, Byte), Integer), CType(CType(10, Byte), Integer))
+        Me.multiplyBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.multiplyBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.multiplyBtn.Location = New System.Drawing.Point(238, 175)
+        Me.multiplyBtn.Name = "multiplyBtn"
+        Me.multiplyBtn.Size = New System.Drawing.Size(56, 56)
+        Me.multiplyBtn.TabIndex = 2
+        Me.multiplyBtn.Text = "*"
+        Me.multiplyBtn.UseVisualStyleBackColor = False
+        '
+        'addBtn
+        '
+        Me.addBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(159, Byte), Integer), CType(CType(10, Byte), Integer))
+        Me.addBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.addBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.addBtn.Location = New System.Drawing.Point(238, 330)
+        Me.addBtn.Name = "addBtn"
+        Me.addBtn.Size = New System.Drawing.Size(56, 56)
+        Me.addBtn.TabIndex = 3
+        Me.addBtn.Text = "+"
+        Me.addBtn.UseVisualStyleBackColor = False
+        '
+        'equalsBtn
+        '
+        Me.equalsBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(159, Byte), Integer), CType(CType(10, Byte), Integer))
+        Me.equalsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.equalsBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.equalsBtn.Location = New System.Drawing.Point(238, 409)
+        Me.equalsBtn.Name = "equalsBtn"
+        Me.equalsBtn.Size = New System.Drawing.Size(56, 56)
+        Me.equalsBtn.TabIndex = 4
+        Me.equalsBtn.Text = "="
+        Me.equalsBtn.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(165, Byte), Integer), CType(CType(165, Byte), Integer), CType(CType(165, Byte), Integer))
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Black
+        Me.Button1.Location = New System.Drawing.Point(167, 103)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(56, 56)
+        Me.Button1.TabIndex = 5
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.btn9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn9.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.Location = New System.Drawing.Point(167, 175)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(56, 56)
+        Me.btn9.TabIndex = 6
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.btn6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn6.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.Location = New System.Drawing.Point(167, 252)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(56, 56)
+        Me.btn6.TabIndex = 7
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.btn3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.Location = New System.Drawing.Point(167, 330)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(56, 56)
+        Me.btn3.TabIndex = 8
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btnDecimal
+        '
+        Me.btnDecimal.BackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.btnDecimal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btnDecimal.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnDecimal.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDecimal.Location = New System.Drawing.Point(167, 409)
+        Me.btnDecimal.Name = "btnDecimal"
+        Me.btnDecimal.Size = New System.Drawing.Size(56, 56)
+        Me.btnDecimal.TabIndex = 9
+        Me.btnDecimal.Text = "."
+        Me.btnDecimal.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.btn2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.Location = New System.Drawing.Point(93, 330)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(56, 56)
+        Me.btn2.TabIndex = 13
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.btn5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn5.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.Location = New System.Drawing.Point(93, 252)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(56, 56)
+        Me.btn5.TabIndex = 12
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.btn8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn8.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.Location = New System.Drawing.Point(93, 175)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(56, 56)
+        Me.btn8.TabIndex = 11
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'delBtn
+        '
+        Me.delBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(165, Byte), Integer), CType(CType(165, Byte), Integer), CType(CType(165, Byte), Integer))
+        Me.delBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.delBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.delBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.delBtn.ForeColor = System.Drawing.Color.Black
+        Me.delBtn.Location = New System.Drawing.Point(93, 103)
+        Me.delBtn.Name = "delBtn"
+        Me.delBtn.Size = New System.Drawing.Size(56, 56)
+        Me.delBtn.TabIndex = 10
+        Me.delBtn.Text = "Del"
+        Me.delBtn.UseVisualStyleBackColor = False
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.btn1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(17, 330)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(56, 56)
+        Me.btn1.TabIndex = 17
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.btn4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn4.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.Location = New System.Drawing.Point(17, 252)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(56, 56)
+        Me.btn4.TabIndex = 16
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.btn7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn7.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.Location = New System.Drawing.Point(17, 175)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(56, 56)
+        Me.btn7.TabIndex = 15
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'delAllBtn
+        '
+        Me.delAllBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(165, Byte), Integer), CType(CType(165, Byte), Integer), CType(CType(165, Byte), Integer))
+        Me.delAllBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.delAllBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.delAllBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.delAllBtn.ForeColor = System.Drawing.Color.Black
+        Me.delAllBtn.Location = New System.Drawing.Point(17, 103)
+        Me.delAllBtn.Name = "delAllBtn"
+        Me.delAllBtn.Size = New System.Drawing.Size(56, 56)
+        Me.delAllBtn.TabIndex = 14
+        Me.delAllBtn.Text = "AC"
+        Me.delAllBtn.UseVisualStyleBackColor = False
+        '
+        'btn0
+        '
+        Me.btn0.BackColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.btn0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btn0.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn0.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn0.Location = New System.Drawing.Point(17, 409)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(132, 56)
+        Me.btn0.TabIndex = 18
+        Me.btn0.Text = "0"
+        Me.btn0.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btn0.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(313, 487)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.delAllBtn)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.delBtn)
+        Me.Controls.Add(Me.btnDecimal)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.equalsBtn)
+        Me.Controls.Add(Me.addBtn)
+        Me.Controls.Add(Me.multiplyBtn)
+        Me.Controls.Add(Me.minusBtn)
+        Me.Controls.Add(Me.divideBtn)
+        Me.Controls.Add(Me.textBox)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents textBox As RichTextBox
+    Friend WithEvents divideBtn As Button
+    Friend WithEvents minusBtn As Button
+    Friend WithEvents multiplyBtn As Button
+    Friend WithEvents addBtn As Button
+    Friend WithEvents equalsBtn As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btnDecimal As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents delBtn As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents delAllBtn As Button
+    Friend WithEvents btn0 As Button
+End Class
---
+++ calculator using vb/Calculator/Form1.vb
@@ -0,0 +1,68 @@
+﻿Public Class Form1
+    Dim currentInput As String
+    Dim firstOperand As Double
+    Dim secondOperand As Double
+    Dim operation As String
+
+    Private Sub NumericButton_Click(sender As Object, e As EventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click
+        Dim button As Button = DirectCast(sender, Button)
+        currentInput &= button.Text
+        textBox.Text = currentInput
+    End Sub
+
+    Private Sub OperatorButton_Click(sender As Object, e As EventArgs) Handles addBtn.Click, minusBtn.Click, multiplyBtn.Click, divideBtn.Click
+        Dim button As Button = DirectCast(sender, Button)
+
+        If Not String.IsNullOrEmpty(currentInput) Then
+            firstOperand = CDbl(currentInput)
+            currentInput = ""
+            operation = button.Text
+        End If
+    End Sub
+
+    Private Sub equalsBtn_Click(sender As Object, e As EventArgs) Handles equalsBtn.Click
+        If Not String.IsNullOrEmpty(currentInput) Then
+            secondOperand = CDbl(currentInput)
+            currentInput = ""
+
+            Select Case operation
+                Case "+"
+                    currentInput = (firstOperand + secondOperand).ToString()
+                Case "-"
+                    currentInput = (firstOperand - secondOperand).ToString()
+                Case "*"
+                    currentInput = (firstOperand * secondOperand).ToString()
+                Case "÷"
+                    If secondOperand <> 0 Then
+                        currentInput = (firstOperand / secondOperand).ToString()
+                    Else
+                        currentInput = "Error"
+                    End If
+            End Select
+
+            textBox.Text = currentInput
+        End If
+    End Sub
+
+    Private Sub btnDecimal_Click(sender As Object, e As EventArgs) Handles btnDecimal.Click
+        If Not currentInput.Contains(".") Then
+            currentInput &= "."
+            textBox.Text = currentInput
+        End If
+    End Sub
+
+    Private Sub delBtn_Click(sender As Object, e As EventArgs) Handles delBtn.Click
+        If currentInput.Length > 0 Then
+            currentInput = currentInput.Substring(0, currentInput.Length - 1)
+            textBox.Text = currentInput
+        End If
+    End Sub
+
+    Private Sub delAllBtn_Click(sender As Object, e As EventArgs) Handles delAllBtn.Click
+        currentInput = ""
+        firstOperand = 0
+        secondOperand = 0
+        operation = ""
+        textBox.Text = ""
+    End Sub
+End Class
